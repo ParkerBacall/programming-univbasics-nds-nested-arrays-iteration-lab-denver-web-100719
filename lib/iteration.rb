094e1f4bca74d4e12c75225b0f1434da7044358d
Parker Bacall <pbacall2@gmail.com>
2019-10-19 19:35:32 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -48,11 +48,12 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     if src[row_index][element_index] % 2 == 0 && src[row_index][element_index+1] % 2 == 0
       total += (src[row_index][element_index] + src[row_index][element_index+1] )
+    end
     element_index += 2
   end
   row_index += 1
 end
- new_array
+ total
 
 
 end
