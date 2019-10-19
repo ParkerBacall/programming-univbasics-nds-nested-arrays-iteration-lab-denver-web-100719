16141da6b71eebf51e22516ac763111a129befb9
Parker Bacall <pbacall2@gmail.com>
2019-10-19 19:28:11 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -20,7 +20,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index] > [row_index][element_index+1]
-    high_value
+    high_value = src[row_index][element_index]
     new_array.push(high_value)
     element_index += 2
   end
